64e6d33a40acf561e0db81db6edf254462db5600
MilanBojic1999 <mbojic12@gmail.com>
2020-03-22 14:12:57 +0100

    Server class added

---
+++ src/Server.java
@@ -0,0 +1,9 @@
+
+import java.net.*;
+public class Server {
+
+    public Server() throws Exception{
+        ServerSocket serverConnectionSocket=new ServerSocket(21);
+        ServerSocket serverTransferSocket=new ServerSocket(20);
+    }
+}
