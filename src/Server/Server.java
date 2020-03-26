3baf40d5fc7b084476989f82035b7f3f8c1f878b
MilanBojic1999 <mbojic12@gmail.com>
2020-03-26 21:02:06 +0100

    Server Thread class added

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -15,73 +15,13 @@ class Server {
 
     public Server() throws Exception{
         utilInit();
-
-        ServerSocket serverConnectionSocket=new ServerSocket(21);
-        ServerSocket serverTransferSocket=new ServerSocket(20);
-        Socket connectSocket;
-        while (true) {
-            if(connectSocket(serverConnectionSocket.accept())){
-                socket=serverTransferSocket.accept();
-                transferSocket(socket);
-                socket.close();
-            }
-        }
-
-
-        //transferSocket.close();
-
+        ServerSocket csSocket=new ServerSocket(21);
+        ServerSocket tsSocket=new ServerSocket(20);
     }
 
-    private boolean connectSocket(Socket socket) throws IOException {
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
 
-        out.println("Log in please!!!");
-        User dummy = new User("1", "1");
-        while (true) {
-            out.println("Enter username: ");
-            dummy.setUsername(in.readLine());
-            out.println("Enter password: ");
-            dummy.setPassword(in.readLine());
-            if (users.contains(dummy) || dummy.getUsername().equalsIgnoreCase("anonymous")) {
-                out.println("Welcome to server");
-                break;
-            } else {
-                out.println("Wrong information");
-            }
-        }
 
-        return true;
-    }
-
-    private boolean transferSocket(Socket socket) throws IOException{
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
-        boolean flag=false;
-        do {
-            out.println("ftp> ");
-            String comm = in.readLine();
-            switch (comm) {
-                case "get":
-                    out.println("Get command");
-                    break;
-                case "put":
-                    put();
-                    out.println("Put command");
-                    break;
-                case "quit":
-                    out.println("Quiting...");
-                    flag = true;
-                    break;
-                default:
-                    out.println("Unknown command");
-            }
-
-        } while (!flag);
-
-        return false;
-    }
 
     private File get(){
         return null;
